ded9fd5de3264d1b34c495300dae01034049e389
wildanptra <wildanptra03@gmail.com>
2022-03-31 12:54:15 +0700

    component button

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+import Button from 'elements/Button' 
+
+export default class LandingPage extends Component {
+  render() {
+    return (
+        <div>LandingPage</div>
+    )
+  }
+}
